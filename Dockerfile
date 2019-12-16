09fd1fe94b43f1c160bfd5fda9d4e35ac26e5ca1
Matias Andrade <mandrade2@uc.cl>
2019-12-15 23:41:03 -0300

    last fixes

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,5 @@
-FROM python:3.5-slim
+FROM python:3.6.5
 
-LABEL maintainer="mandrade2@uc.cl"
-
-USER root
-
-WORKDIR /app
-
-ADD . /app
-
-RUN pip install  -r requirements.txt
-
-EXPOSE 5000
-
-ENV NAME World
-
-# CMD ["gunicorn -k eventlet -w 1" ," module:app"]
-CMD ["python" ,"chatapp.py"]
+WORKDIR /code
+COPY * /code/
+RUN pip install -r requirements.txt
--- chatapp.py
+++ chatapp.py
@@ -37,16 +37,29 @@ def response(value):
 
 @socketio.on('tell-joke')
 def joke():
-    url = "https://api.chucknorris.io/jokes/random"
+    url = "https://n2sst0bbr8.execute-api.us-east-1.amazonaws.com/default/MyTestFunction"
     response = requests.request("GET", url)
-    text = json.loads(response.text)
-    value = text['value']
-    print(value)
-    socketio.emit('joke', value)
+    joke = response.text[1:-1]
+    print(joke)
+    socketio.emit('joke', joke)
+
+
+@socketio.on('get-name')
+def name():
+    url = "https://n2sst0bbr8.execute-api.us-east-1.amazonaws.com/default/testFunction"
+    response = requests.request("GET", url)
+    name = response.text[1:-1]
+    print(name)
+    socketio.emit("name", name)
 
 
 @socketio.on('send-message')
 def send_message(msg):
+    print(msg)
+    # url = "https://n2sst0bbr8.execute-api.us-east-1.amazonaws.com/default/lastFunction"
+    # response = requests.request('PUT', url, data=json.dumps({"message": msg}))
+    # print(response.text)
+    # msg = response.text[1:-1]
     print("request", request.sid)
     time1 = time.localtime()
     format_time = str(time1.tm_hour) + ':' + str(time1.tm_min) + ' ' + \
@@ -87,4 +100,4 @@ def test_disconnect():
 
 if __name__ == '__main__':
 
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True,host='0.0.0.0')
